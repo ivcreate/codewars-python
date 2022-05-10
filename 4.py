5c19ba09d1ba9302de68fad7a9fda3c2348c6b04
ivcreate <beelineivanobl@gmail.com>
2022-05-10 22:36:46 +0300

    Start 4

---
+++ 4.py
@@ -0,0 +1,23 @@
+# Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+#
+# The binary number returned should be a string.
+#
+# Examples:(Input1, Input2 --> Output (explanation)))
+#
+# 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+# 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+
+import codewars_test as test
+
+def add_binary(a,b):
+    return
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(add_binary(1,1),"10")
+        test.assert_equals(add_binary(0,1),"1")
+        test.assert_equals(add_binary(1,0),"1")
+        test.assert_equals(add_binary(2,2),"100")
+        test.assert_equals(add_binary(51,12),"111111")
